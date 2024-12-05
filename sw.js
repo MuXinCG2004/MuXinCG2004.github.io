9b432aa42b3ebcbde4de6988592456717718cf45
MuXinCG <202322130196@mail.sdu.edu.cn>
2024-12-05 09:31:31 +0800

    Site updated: 2024-12-05 09:31:31

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733362147437";
+const VERSION = "1733362253668";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
